1f28c39fde5d87ad926c7f4c56faa910a8ff070a
Atil Samancioglu <samancioglu.atil@gmail.com>
2023-10-26 17:16:45 +0300

    Initial Commit

---
+++ SwiftUILandmark/SwiftUILandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILandmarkApp.swift
+//  SwiftUILandmark
+//
+//  Created by Atil Samancioglu on 26.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
